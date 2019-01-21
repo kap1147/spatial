b263d0f1e9523cb875bb6e9b4e9261a925905c83
kap1147 <joshua1147.ad@gmail.com>
2019-01-21 23:50:31 +0000

    add username to post api

--- post/serializers.py
+++ post/serializers.py
@@ -1,9 +1,19 @@
 from .models import Post
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 # serializers define the API representation.
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+
+        fields = ('username',)
+
 class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
     class Meta:
         model = Post
-        fields = ('title', 'content', 'timestamp','location')
+
+        fields = ('title', 'content', 'timestamp', 'user', 'location')
 
